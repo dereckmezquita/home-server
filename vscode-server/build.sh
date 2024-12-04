367871af3b7273784297658759a726fa6ebac34c
dereckmezquita <dereck@mezquita.io>
2024-12-03 21:53:14 -0600

    Runs vscode server.

--- vscode-server/build.sh
+++ vscode-server/build.sh
@@ -1,3 +1,9 @@
-docker build -t vscode-server .
-docker compose up -d
-docker compose exec vscode-server bash
+# Stop and remove the running containers for vscode-server
+docker compose down
+
+# Clean up unused containers and images
+docker container prune -f
+docker image prune -f
+
+# Build and run the updated vscode-server
+docker compose up --build -d
